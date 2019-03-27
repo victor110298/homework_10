c7ebaf01ec77095d0b40a7ef5b8a779dabb3da34
victor110298 <44604893+victor110298@users.noreply.github.com>
2019-03-28 00:16:45 +0200

    Fixed

--- src/main/java/xmlExpression/UtilTest.java
+++ src/main/java/xmlExpression/UtilTest.java
@@ -1,36 +1,39 @@
 package xmlExpression;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.*;
 
 public class UtilTest {
+
     private UtilTask task;
+
+    @Before
+    public void initial() {
+        task = new UtilTask();
+    }
 
     @Test
     public void validNumber() {
-        task = new UtilTask();
         String number = "+38041225235";
         assertTrue(task.validNumber(number));
     }
 
     @Test
     public void validEmail() {
-        task = new UtilTask();
         String email = "egor_ka@delaldz.com";
         assertTrue(task.validEmail(email));
     }
 
     @Test
     public void validDate() {
-        task = new UtilTask();
         String date = "11-02-1998";
         assertFalse(task.validDate(date));
     }
 
     @Test
     public void validIp() {
-        task = new UtilTask();
         String ip = "192.168.0.1";
         assertTrue(task.validIp(ip));
     }
